270b0ba5ef184635561700249c370698bab25a93
nokoboi <msnrelleno1@hotmail.com>
2022-03-24 21:50:49 +0100

    Commit desde mi casa

--- Tetris2/src/tetris2/Tetris2.java
+++ Tetris2/src/tetris2/Tetris2.java
@@ -6,6 +6,8 @@ public class Tetris2 {
     public static void main(String[] args) {
         System.out.println("El proyector tiene alma");
         System.out.println("Hola mundo");
+        
+        System.out.println("Esto es una modificación desde mi casa. Hola Kevin del mañana");
     }
     
 }
